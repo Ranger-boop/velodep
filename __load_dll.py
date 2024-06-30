4d9c6275b96b870961dc29aea38ea495dc0e0bf5
Supeng Zhang <suppengzhang@gmail.com>
2024-06-30 20:41:53 +0200

    Set a cmake switch to optionally pack system dependencies with the released packages.

--- __load_dll.py
+++ __load_dll.py
@@ -8,11 +8,11 @@ STATUS = "Develop"
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if STATUS == "Develop":
     if os.name == "nt":
-        # dll_path = os.path.join(current_dir, "build/bin/Debug/velodep.dll")
-        dll_path = os.path.join(current_dir, "build/bin/Release/velodep.dll")
+        # dll_path = os.path.join(current_dir, "build/Debug/bin/velodep.dll")
+        dll_path = os.path.join(current_dir, "build/Release/bin/velodep.dll")
     else:
-        # dll_path = os.path.join(current_dir, "build/lib/Debug/libvelodep.so")
-        dll_path = os.path.join(current_dir, "build/lib/Release/libvelodep.so")
+        # dll_path = os.path.join(current_dir, "build/Debug/lib/libvelodep.so")
+        dll_path = os.path.join(current_dir, "build/Release/lib/libvelodep.so")
 elif STATUS == "Release":
     if os.name == "nt":
         dll_path = os.path.join(current_dir, "bin/velodep.dll")
